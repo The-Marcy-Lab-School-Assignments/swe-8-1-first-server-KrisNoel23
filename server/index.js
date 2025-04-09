08150789f4cc5c58a113b3de0eaa3d5f6ae06f6d
Marcy Student <Marcy_Student@MARCY-MBAir18.local>
2025-04-09 19:37:00 -0400

    I made some changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
+
+const filepath = path.join(__dirname, '../App/dist');
+const serveStatic = express.static(filepath);
 
 const servePicture = (req, res) => {
     res.send({src: "https://static-cdn.jtvnw.net/jtv_user_pictures/meowntain-profile_banner-71b7a6d0d943dc9e-480.jpeg"});
@@ -22,8 +25,6 @@ const serveRoll = (req, res) => {
 };
 
 
-
-
 const logRoutes = (req, res, next) => {
     const time = (new Date()).toLocaleString();
     req.time = time;
@@ -31,21 +32,10 @@ const logRoutes = (req, res, next) => {
     next();
   };
 
-  // The path module is useful for constructing relative filepaths
-const path = require('path');
 
-// the filepath is to the entire assets folder
-const filepath = path.join(__dirname, '../vite-project/dist');
-
-// generate middleware using the filepath
-const serveStatic = express.static(filepath);
-
-// Register the serveStatic middleware before the remaining controllers
-app.use(serveStatic);
-
-// other controllers 
 
 app.use(logRoutes);
+app.use(serveStatic);
 
 
 app.get('/api/picture', servePicture);
